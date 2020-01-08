ac6b66ec6b05ce16d2cabbe59bbca71010b51990
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-08 22:30:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,7 +50,8 @@ class Owner
     def sell_pets
       Dog.all.each {|dog| dog.mood = "nervous"}
       Cat.all.each {|cat| cat.mood = "nervous"}
-      Dog.owner = nil
+      Dog.all.clear
+      Cat.all.clear
       
     end 
 end
